a118501cc43ac1e8efd34ca39a25f26899ffb918
Mike Berman <mike.berman@kizzang.com>
2015-04-03 15:59:42 -0700

    Complete rewrite

--- getInstanceId.py
+++ getInstanceId.py
@@ -1,80 +1,42 @@
 #!/usr/bin/python -tt
 
-import sys,getopt
-import commands 
-import logging
+import sys
+import boto.utils
 import json
-
-logger = logging.getLogger('stencil')
-hdlr = logging.StreamHandler(sys.stdout)
-#hdlr = logging.FileHandler('stencil.log') 
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
-logger.setLevel(logging.INFO) #logging.DEBUG
-
-
-def help():
-
-  print " Usage: getInstanceId.py > instance.json [--help] "
-
-
-
-def Run(cmd):
  
-  logger.debug("** Running: %s" % cmd)
+class AwsInstanceDetails(object):
 
-  (status,output) = commands.getstatusoutput(cmd)
+  def __init__(self):
 
-  if status > 0:
-    logger.error(output)
-    sys.exit(2)
+    try:
+      self.instance = boto.utils.get_instance_metadata() 
+    except Exception as e:
+      print "FATAL: Could not grab instance information from boto"
+      sys.exit(2)
  
-  logger.debug('[OK]')
-  return output
 
+  def getPublicHost(self):
+
+    return self.instance['public-hostname']
+ 
+  def getInstanceId(self):
+
+    return self.instance['instance-id']
 
 
 def main(argv):
 
-  # make sure command line arguments are valid
-  try:
-    options, args = getopt.getopt(
-
-       argv, 
-      'hv', 
-      [ 
-        'help',
-        'verbose' 
-    
-      ])
- 
-  except getopt.GetoptError:
-    logging.fatal("Bad options!")
-    help()
-    sys.exit(2)
-
-
-  # handle command line arugments
-  for opt, arg in options:
-    if opt in ('-h', '--help'):
-      help()
-      sys.exit(2)
-    elif opt in ('-v', '--verbose'):
-      logger.setLevel(logging.DEBUG) 
- 
-
-  ###################################
-  # main code starts here
-  ###################################
-
-  cmd = "wget --timeout 3 --tries 1 -q -O - http://169.254.169.254/latest/meta-data/instance-id"
-  instance_id = Run(cmd) 
-
+  ###########################################
+  # some test code, never runs when included
+  # This must be run from an AWS box
+  ###########################################
+  
+  k = AwsInstanceDetails()
+  
   j = {
 
-    "instanceId":instance_id
-
+    "public-host": k.getPublicHost(),
+    "instance-id": k.getInstanceId()
   }
 
   print json.dumps(j)
@@ -82,8 +44,3 @@ def main(argv):
 
 if __name__ == "__main__":
   main(sys.argv[1:])
- 
-
- 
-
-
